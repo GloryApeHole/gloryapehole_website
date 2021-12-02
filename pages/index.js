caece80d30afb117e82c846975af5dd3883c5657
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-02 16:53:42 +0200

    added meta

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,19 @@ function Home() {
 Sale starts soon. Don't miss it! 
 "
         />
+        <meta
+          property='og:description'
+          content="6969 Glory Ape Hole NFTs. Sale starts soon. Don't miss it!"
+        />
+        <meta
+          name='twitter:description'
+          content="6969 Glory Ape Hole NFTs. Sale starts soon. Don't miss it! "
+        />
+        <meta property='og:image' content={'/gah_cover.jpg'} />
+        <meta name='twitter:image' content={'/gah_cover.jpg'} />
+        <meta name='image' content={'/gah_cover.jpg'} />
+        <meta property='og:title' content={'Glory Ape Hole'} />
+        <meta name='twitter:title' content={'Glory Ape Hole'} />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className='flex flex-col items-center min-h-screen py-2'>
