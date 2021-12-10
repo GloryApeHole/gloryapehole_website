a8b8be1ce60a2adcb520d4d651f34d8698de4322
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-10 15:30:00 +0200

    hotjar'

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import { hotjar } from 'react-hotjar';
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(2742673, 6);
+  }, []);
   return <Component {...pageProps} />;
 }
 
