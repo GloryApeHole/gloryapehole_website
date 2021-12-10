47c7ad80e116a06b8fe8f8f3399a644d6abcff66
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-10 10:00:54 +0200

    fixes

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -2,7 +2,7 @@ import Countdown from 'components/countdown';
 export default function Hero() {
   return (
     <div className='min-h-screen text-white hero-background font-bold h-full w-full flex justify-start items-center flex-col'>
-      <h1 className='mt-32 text-6xl text-center mb-3'>
+      <h1 className='mt-32 hero-title text-6xl text-center mb-3'>
         PRE-SALE HAPPENING NOW
       </h1>
       <h1 className='mt-20 text-4xl text-center mb-3'>CLAIM YOURS</h1>
