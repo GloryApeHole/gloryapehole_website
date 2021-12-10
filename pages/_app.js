4199ecad1f0090c262c55d12b910b9a080b46994
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-10 15:35:51 +0200

    added tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,15 @@ import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import { hotjar } from 'react-hotjar';
 import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-N2STVGF',
+};
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
     hotjar.initialize(2742673, 6);
   }, []);
   return <Component {...pageProps} />;
